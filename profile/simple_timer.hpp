0c4e9e35a9e3a06ae92c4c25b445092dde64cc2c
“proton991” <99615819@qq.com>
2022-12-17 15:52:02 +0800

    implement simple timer for measuring execution time

---
+++ profile/simple_timer.hpp
@@ -0,0 +1,27 @@
+#ifndef EASYCPP_SIMPLE_TIMER_HPP
+#define EASYCPP_SIMPLE_TIMER_HPP
+
+#include <chrono>
+class SimpleTimer {
+
+
+public:
+  SimpleTimer() {
+    m_last_time = std::chrono::high_resolution_clock::now();
+  }
+  [[nodiscard]] float TimeStep() {
+    const auto currentTime = std::chrono::high_resolution_clock::now();
+
+    auto time_duration = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - m_last_time).count();
+
+    m_last_time = currentTime;
+
+    return time_duration;
+  }
+private:
+  // The time point of the last render call.
+  std::chrono::time_point<std::chrono::high_resolution_clock> m_last_time;
+
+};
+
+#endif  //EASYCPP_SIMPLE_TIMER_HPP
--- tests/sorting_tests.cpp
+++ tests/sorting_tests.cpp
@@ -4,6 +4,7 @@
 #include "heap_sort.hpp"
 #include "bubble_sort.hpp"
 #include "printer.hpp"
+#include "simple_timer.hpp"
 
 TEST_CASE("Test random vector generation") {
   std::vector<int> randomVector = common::generate_random_int_vector(10);
@@ -24,3 +25,13 @@ TEST_CASE("Test bubble sort asc") {
   common::print_array(randomVector.data(), randomVector.size());
 }
 
+TEST_CASE("Test simple timer") {
+  std::vector<int> randomVector = common::generate_random_int_vector(1000);
+  SimpleTimer timer;
+  heap_sort(randomVector.data(), randomVector.size());
+  std::cout << "heap sort time used: " << timer.TimeStep() << "ms\n";
+
+  std::vector<int> randomVector2 = common::generate_random_int_vector(1000);
+  bubble_sort(randomVector2.data(), randomVector2.size());
+  std::cout << "bubble sort time used: " << timer.TimeStep() << "ms\n";
+}
